1e007e391a91ea10be3d38dcf9045e51db9d91fc
THE-NOS44 <44903050+THE-NOS44@users.noreply.github.com>
2022-06-29 23:03:32 +0530

    Update Merge_Sorted_Array.py

--- Merge_Sorted_Array.py
+++ Merge_Sorted_Array.py
@@ -1,3 +1,6 @@
+        '''Video Link - https://www.youtube.com/watch?v=C4oBXLr3zos'''
+
+        
         # - 2 Pointer Approach ( REMEMBER --> ALREADY SORTED ARRAYS)
   
         # - Question says that nums1 array is actually having 0s at the end , which is equal to the length of nums2
@@ -36,11 +39,4 @@
             
          
         
- '''       
-Video Link - https://www.youtube.com/watch?v=C4oBXLr3zos
-
-![image](https://user-images.githubusercontent.com/44903050/176498838-dcb3e1d2-b730-45c3-9911-f9e06fed9838.png)
-![image](https://user-images.githubusercontent.com/44903050/176499059-37d491e9-4d23-4d3c-9c0b-b75149d02586.png)
-![image](https://user-images.githubusercontent.com/44903050/176499137-7f1edc21-9c29-4b63-b7fa-de5d03acc317.png)
-
-'''
+    
