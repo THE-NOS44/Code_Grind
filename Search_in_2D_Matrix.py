8bc072130ee7c968bc27f89b8c5aef685ee75f05
THE-NOS44 <44903050+THE-NOS44@users.noreply.github.com>
2022-07-04 20:54:03 +0530

    Create Search_in_2D_Matrix.py

---
+++ Search_in_2D_Matrix.py
@@ -0,0 +1,37 @@
+# LC - 74
+
+# 2 Approaches - BINARY Search Ususally - done below
+#              - 1 more better approach is linked - https://www.youtube.com/watch?v=ZYpYur0znng&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=15&ab_channel=takeUforward
+#              - In that other approach we take a the last element of the first row as Start point , now if the target is smaller than the CURRENT , we move to the RIGHT 
+#              - and if the target is greater than the CURRENT , we move one step DOWN . 
+#              - We keep doing this until we find our target or we go out of bounds
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        # BINARY SEARCH
+        
+        n = len(matrix)
+        m = len(matrix [0])
+        
+        # we are taking the LOW and HIGH as if the whole matrix was a single continuous array.
+        low = 0
+        high = (m*n) - 1
+        
+        
+        while low <= high :
+            
+            # below we find the MID with overflow prevention
+            mid = low +(high -low) //2 
+            
+   #--->    # NOW this expression below actually takes mid value which is the MID if we took the matrix as an               
+            # array , and converts it into an index value of the matrix in [i][j] form . 
+            # Rest is simple BINARY SEARCH
+      
+            if ( matrix[mid//m][mid%m] == target ):
+                return True
+            elif (matrix[mid//m][mid%m] < target) :
+                low = mid + 1
+            else:
+                high = mid -1        
+        return False
